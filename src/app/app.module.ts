83d3af797309e7275957993c8579f943bf93fbf8
Danilo Ferreira <danilocsf@gmail.com>
2019-04-16 22:07:45 -0300

    Estrutura inicial do modulo de renda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/header/header.component';
-
+import { ListIncomeComponent } from './modules/incomes/list/list-income.component';
+import { DetailIncomeComponent } from './modules/incomes/detail/detail-income.component';
+import { CreateUpdateIncomeComponent } from './modules/incomes/create-update/create-update-income.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListIncomeComponent,
+    DetailIncomeComponent,
+    CreateUpdateIncomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modules/incomes/create-update/create-update-income.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-income',
+  templateUrl: './create-update-income.component.html',
+  styleUrls: ['./create-update-income.component.css']
+})
+export class CreateUpdateIncomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
